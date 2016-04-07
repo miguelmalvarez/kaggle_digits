2e180bc91bd0147903d2e1fea911538f3582b355
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2016-04-07 08:11:30 +0100

    Ensemble classifier added

--- src/run.py
+++ src/run.py
@@ -5,6 +5,7 @@ import time
 import datetime
 import os
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
 from sklearn import cross_validation
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import LinearSVC
@@ -54,10 +55,16 @@ def candidate_families():
     candidates = []
     svm_tuned_parameters = [{'kernel': ['poly'], 'degree': [1, 2, 3, 4]}]
     candidates.append(["SVM", SVC(C=1), svm_tuned_parameters])
-    rf_tuned_parameters = [{"n_estimators": [100, 250, 500, 1000]}]
+    rf_tuned_parameters = [{"n_estimators": [100, 250, 500]}]
     candidates.append(["RandomForest", RandomForestClassifier(n_jobs=-1), rf_tuned_parameters])        
     knn_tuned_parameters = [{"n_neighbors": [1, 3, 5, 10, 20]}]
-    candidates.append(["kNN", KNeighborsClassifier(), knn_tuned_parameters])
+    candidates.append(["kNN", KNeighborsClassifier(), knn_tuned_parameters])    
+    ensemble_tuned_parameters = [{"voting": ['hard']}]
+    ensemble = VotingClassifier(estimators=[
+        ('SVM', SVC(C=1, degree=2, kernel='poly')), 
+        ('RF', RandomForestClassifier(n_jobs=-1, n_estimators=100)), 
+        ('kNN', KNeighborsClassifier(n_neighbors=3))])
+    candidates.append(["Ensemble", ensemble, ensemble_tuned_parameters])
     return candidates
 
 # Fitting a feature selector 
@@ -102,7 +109,7 @@ def run(scaler, output_path):
     log_info('============== \nClassification started... ')
 
     log_info('Reading training data... ')
-    train_data = pd.read_csv('data/train.csv', header=0).values
+    train_data = pd.read_csv('data/train-sample-1000.csv', header=0).values
     #the first column of the training set will be the judgements
     judgements = np.array([str(int (x[0])) for x in train_data])
     train_instances = np.array([x[1:] for x in train_data])
@@ -140,8 +147,8 @@ def run(scaler, output_path):
 
 def main():
     init_logging('./logs/', current_timestamp()+'.log')
-    run(MinMaxScaler(), 'data/results-scaling-minmax.csv')
-    run(StandardScaler(), 'data/results-scaling-std.csv')
+    #run(MinMaxScaler(), 'data/results-scaling-minmax.csv')
+    #run(StandardScaler(), 'data/results-scaling-std.csv')
     run(None, 'data/results-no-scaling.csv')    
 
 if __name__=='__main__':
