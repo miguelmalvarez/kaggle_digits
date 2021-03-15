1bbf4a75f08cd25a0786277149fd8c3210b37333
Miguel <miguelmalvarez@gmail.com>
2021-03-15 11:23:32 +0000

    Super simple CNN

--- run_nn.py
+++ run_nn.py
@@ -7,19 +7,26 @@ def main():
     test_data = pd.read_csv('data/test.csv', header=0).values
 
     # Code to do some sampling for quick experimentation
-    sample = 50000
+    sample = 100000
     train_data = train_data[:sample]
     test_data = test_data[:sample]
-    y_train = np.array([int(x[0]) for x in train_data[:sample]])
+    y_train = np.array([[int(x[0])] for x in train_data])
     # --------
 
     # Normalise values
     x_train = np.array([x[1:] for x in train_data]) / 255.0
     x_test = np.array([x for x in test_data]) / 255.0
 
+    # Reshape
+    x_train = x_train.reshape(-1, 28, 28, 1,)
+    x_test = x_test.reshape(-1, 28, 28, 1, )
+    print(f'X_train shape {x_train.shape}')
+    print(f'y_train shape {y_train.shape}')
+
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(784),
-        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Conv2D(32, kernel_size=(5,5), activation='relu', input_shape=(28,28,1)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(10, activation='softmax')
     ])
 
